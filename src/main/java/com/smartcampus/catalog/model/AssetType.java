d656b62a777db9bc20a9298bd17fe2ea366c64e8
hasinilakshika <hasinilakshika56@gmail.com>
2026-04-01 23:32:39 +0530

    feat(model): add AssetType model for managing asset types in the catalog

---
+++ src/main/java/com/smartcampus/catalog/model/AssetType.java
@@ -0,0 +1,41 @@
+package com.smartcampus.catalog.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "asset_types")
+public class AssetType {
+
+    @Id
+    private String id;
+
+    @Indexed(unique = true)
+    private String code;
+
+    private String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
